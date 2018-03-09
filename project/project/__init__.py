bd86403ac2a3262e710cc7d93ee324b93bafe0ab
UPV-DA31-12\admin <dm3-1-02.17@uni.eus>
2018-03-09 12:32:15 +0100

    terminando un par de clases

--- project/project/__init__.py
+++ project/project/__init__.py
@@ -5,4 +5,14 @@ class Produktuak:
         self.prezioa=prezioa
         self.kompainia=kompainia
 class Jokoak(Produktuak):
-    
+    def _init_(self,generoa,data, produktuID, izena, prezioa, kompainia):
+        self.generoa=generoa
+        self.data=data
+        Produktuak.__init__(self, produktuID, izena, prezioa, kompainia)
+class kontsolak(Produktuak):
+    def _init_(self,jokoak, produktuID, izena, prezioa, kompainia):
+        self.jokoak=jokoak
+        Produktuak.__init__(self, produktuID, izena, prezioa, kompainia)
+class osagarriak(Produktuak):
+    def __init__(self, produktuID, izena, prezioa, kompainia):
+        Produktuak.__init__(self, produktuID, izena, prezioa, kompainia)
