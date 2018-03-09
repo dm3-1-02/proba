a18bd06e5462699901f61d2beb24aaa1206f4852
UPV-DA31-12\admin <dm3-1-02.17@uni.eus>
2018-03-09 12:18:18 +0100

    primera prueba

---
+++ project/project/__init__.py
@@ -0,0 +1,8 @@
+class Produktuak:
+    def __init__(self,produktuID,izena,prezioa,kompainia):
+        self.produktuID=produktuID
+        self.izena=izena
+        self.prezioa=prezioa
+        self.kompainia=kompainia
+class Jokoak(Produktuak):
+    
